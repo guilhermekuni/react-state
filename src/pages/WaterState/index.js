74e23cdb679cb49c4073e4b02cd1b439c0022d77
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-06-08 23:06:58 -0300

    implements water state validation

--- src/pages/WaterState/index.js
+++ src/pages/WaterState/index.js
@@ -1,4 +1,6 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
+import Slider from 'react-rangeslider'
+import 'react-rangeslider/lib/index.css'
 
 import waterGas from '../../assets/images/water-gas.png';
 import waterLiquid from '../../assets/images/water-liquid.png';
@@ -6,28 +8,46 @@ import waterSolid from '../../assets/images/water-solid.png';
 
 import * as S from './styles';
 
+const WATER_STATES = {
+  GAS: 'gas',
+  LIQUID: 'liquid',
+  SOLID: 'solid',
+}
+
 const WATER_STATES_IMAGES = {
-  'gas': waterGas,
-  'liquid': waterLiquid,
-  'solid': waterSolid,
+  [WATER_STATES.GAS]: waterGas,
+  [WATER_STATES.LIQUID]: waterLiquid,
+  [WATER_STATES.SOLID]: waterSolid,
 };
 
-const WATER_STATES = {
-  'gas': 'Gasoso',
-  'liquid': 'Líquido',
-  'solid': 'Sólido',
+const WATER_STATES_LABELS = {
+  [WATER_STATES.GAS]: 'Gasoso',
+  [WATER_STATES.LIQUID]: 'Líquido',
+  [WATER_STATES.SOLID]: 'Sólido',
 };
 
 const WaterState = () => {
   const [waterState, setWaterState] = useState('gas');
-  const [temperature, setTemperature] = useState('gas');
+  const [temperature, setTemperature] = useState(150);
+
+  useEffect(() => {
+    const { GAS, LIQUID, SOLID } = WATER_STATES;
+
+    if (temperature >= 100) {
+      setWaterState(GAS);
+    } else if (temperature >= 0) {
+      setWaterState(LIQUID);
+    } else {
+      setWaterState(SOLID);
+    }
+  }, [temperature]);
 
   const waterStateImage = useMemo(() =>
     WATER_STATES_IMAGES[waterState],
     [waterState]);
 
   const waterStateName = useMemo(() =>
-    WATER_STATES[waterState],
+    WATER_STATES_LABELS[waterState],
     [waterState]);
 
   return (
@@ -35,6 +55,20 @@ const WaterState = () => {
       <S.WaterStateWrapper>
         <h2>Estado: {waterStateName}</h2>
         <img alt="Water State" src={waterStateImage} />
+        <div className='slider orientation-reversed'>
+          <div className='slider-group'>
+            <div className='slider-vertical'>
+              <Slider
+                min={-100}
+                max={200}
+                value={temperature}
+                orientation='vertical'
+                onChange={setTemperature}
+              />
+              <div className='value'>{temperature}°</div>
+            </div>
+          </div>
+        </div>
       </S.WaterStateWrapper>
     </S.Wrapper>
   );
